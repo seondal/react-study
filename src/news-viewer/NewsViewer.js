db2b41a2955fcc66b54c67653832598b8170ef6a
seondal <sseondal@gmail.com>
2021-11-24 15:25:33 +0900

    #2 카테고리 선택 UI 만들기

--- src/news-viewer/NewsViewer.js
+++ src/news-viewer/NewsViewer.js
@@ -1,40 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
 import NewsList from "./NewsList";
+import Categories from "./Categories";
 
 const NewsViewer = () => {
-  const [data, setData] = useState(null);
-  //   const onClick = () => {
-  //     axios
-  //       .get("https://jsonplaceholder.typicode.com/todos/1")
-  //       .then((response) => {
-  //         setData(response.data);
-  //       });
-  //   };
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "http://newsapi.org/v2/top-headlines?country=kr&apiKey=648ef3c02f084600935fa77d2aa7bb11"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
+
   return (
-    // <div>
-    //   <div>
-    //     <button onClick={onClick}>불러오기</button>
-    //   </div>
-    //   {data && (
-    //     <textarea
-    //       rows={7}
-    //       value={JSON.stringify(data, null, 2)}
-    //       readOnly={true}
-    //     />
-    //   )}
-    // </div>
-    <NewsList />
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
 };
 
